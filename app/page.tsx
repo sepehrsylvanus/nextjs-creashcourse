1f69dc1d4a91d168b5af17eafbdff48e3de1f4ee
sepehrsylvanus <sepehr.haghdoust@gmail.com>
2025-11-13 21:36:45 +0300

    porject setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 const page = () => {
-  return <div>Welcome to next.js 16!</div>;
+  return (
+    <section>
+      <h1 className="text-center">
+        {" "}
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in one place
+      </p>
+    </section>
+  );
 };
 
 export default page;
