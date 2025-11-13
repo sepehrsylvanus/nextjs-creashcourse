491f8dc951e68649742ec1473c96619b4f26d5d2
sepehrsylvanus <sepehr.haghdoust@gmail.com>
2025-11-13 23:53:47 +0300

    Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
+
 const page = () => {
   return (
     <section>
@@ -8,6 +12,19 @@ const page = () => {
       <p className="text-center mt-5">
         Hackathons, Meetups, and Conferences, All in one place
       </p>
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
